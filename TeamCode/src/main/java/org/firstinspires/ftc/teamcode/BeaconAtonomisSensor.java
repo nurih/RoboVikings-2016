c2cd612c3fa4435352aacea6d32c119acd9400aa
coder <coders@robovikings9887.com>
2016-11-05 09:30:45 -0700

    Adding autonomous color sensor for beacon

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconAtonomisSensor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BeaconAtonomisSensor.java
@@ -1,27 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.ColorSensor;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
-/**
- * Created by Robovikings on 10/29/2016.
- */
+@TeleOp(name = "Beacon Color Sensor", group = "Mini Ops")
+
 public class BeaconAtonomisSensor extends OpMode {
     public ColorSensor color = null;
+
     @Override
     public void init() {
+
         color = hardwareMap.colorSensor.get("colorsensor1");
+        //passive mode
+        color.enableLed(false);
     }
 
     @Override
     public void loop() {
-        if (color.red() > 1) {
+        if (color.red() > 128) {
             //red
-        } else if (color.blue() > 1) {
+            telemetry.addLine("Red Works");
+        } else if (color.blue() > 128) {
             //blue
-            }
+            telemetry.addLine("Blue Works");
         }
+
     }
+}
 
