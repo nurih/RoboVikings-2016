830467bf142b28478fb596276ceb4cef495d6377
Robovikings <chip.marklar@gmail.com>
2016-12-03 09:36:35 -0800

    scoop position

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scoop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scoop.java
@@ -6,8 +6,8 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 @TeleOp(name = "Scoop", group = "Mini Op")
 public class Scoop extends OpMode {
-    private final double startingPosition = 0.65;
-    private final double finalPosition = 0;
+    private final double startingPosition = Servo.MIN_POSITION;
+    private final double finalPosition = Servo.MAX_POSITION * 0.65;
 
     public Servo scoopServo = null;
 
